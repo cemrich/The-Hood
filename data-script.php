fc0c9f36cef6b3ba19ae48f92da22c093bca0b5d
Christine Emrich <christine.emrich@web.de>
2020-10-15 17:09:02 +0200

    Display only posts from loaded page

--- data-script.php
+++ data-script.php
@@ -1,17 +1,8 @@
 <?php
 
-// TODO: display only posts from loaded page
-$args = array( 
-    'post_type' => 'thehood_location', 
-    'post_status' => 'publish', 
-    'nopaging' => true 
-);
-$query = new WP_Query($args);
-$posts = $query->get_posts();
-
 $arr = array();
 
-foreach ($posts as $post) {
+while ( have_posts() ) : the_post(); 
     $post_meta = get_post_meta($post->ID);
     $arr[] = (object) [
         'id' => $post->ID,
@@ -19,7 +10,7 @@ foreach ($posts as $post) {
         'lat' => $post_meta['thehood_meta_pos_lat'][0],
         'lon' => $post_meta['thehood_meta_pos_lon'][0]
     ];
-}
+endwhile; 
 
 ?>
 
