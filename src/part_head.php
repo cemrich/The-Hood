cb9777953bc4abeb6739cf485c0a360c25a9bb52
Christine Emrich <christine.emrich@web.de>
2020-10-18 16:18:00 +0200

    Improve styling

--- src/part_head.php
+++ src/part_head.php
@@ -1,11 +1,10 @@
 <?php 
 
-wp_enqueue_style('style', get_template_directory_uri().'/style.css'); 
-
-wp_enqueue_style('map-style', get_template_directory_uri().'/css/map.css');
-
 wp_enqueue_style('leaflet-style', get_template_directory_uri().'/vendor/leaflet/leaflet.css');
 wp_enqueue_script('leaflet-script', get_template_directory_uri().'/vendor/leaflet/leaflet.js', null, false, true);
+
+wp_enqueue_style('map-style', get_template_directory_uri().'/css/map.css');
+wp_enqueue_style('style', get_template_directory_uri().'/style.css');
 
 wp_enqueue_script('script', get_template_directory_uri().'/js/script.js', null, false, true);
 
